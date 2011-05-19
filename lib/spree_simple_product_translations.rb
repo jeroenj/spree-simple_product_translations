7ec2812b0ab170530135249ad2434dd3ee8cec7c
Jeroen Jacobs <info@jeroenj.be>
2011-05-19 14:10:19 +0200

    No longer ignoring empty option values in the option types form. The validations will to their work now. This was breaking option value translations

--- lib/spree_simple_product_translations.rb
+++ lib/spree_simple_product_translations.rb
@@ -28,10 +28,12 @@ module SpreeSimpleProductTranslations
 
       ::OptionType.class_eval do
         accepts_nested_attributes_for :translations
+        accepts_nested_attributes_for :option_values, :allow_destroy => true
       end
 
       ::OptionValue.class_eval do
         accepts_nested_attributes_for :translations
+        validates_presence_of :name, :presentation
       end
     end
 
